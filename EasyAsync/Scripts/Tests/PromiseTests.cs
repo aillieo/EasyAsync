6fa3b5c35c2bafcb9fc8018400aaf871b5495e3c
aillieo <aillieo@163.com>
2022-04-16 23:12:05 +0800

    update

--- EasyAsync/Scripts/Tests/PromiseTests.cs
+++ EasyAsync/Scripts/Tests/PromiseTests.cs
@@ -13,11 +13,14 @@ namespace AillieoUtils.EasyAsync.Tests
         public static void TestSimpleResolve()
         {
             int i = 0;
+            int j = 0;
             Promise promise = new Promise();
             promise.OnFulfilled(() => i++);
+            promise.OnRejected(() => j++);
             Assert.AreEqual(i, 0);
             promise.Resolve();
             Assert.AreEqual(i, 1);
+            Assert.AreEqual(j, 0);
             Assert.Catch(typeof(Exception), () => promise.Resolve());
         }
 
@@ -26,13 +29,21 @@ namespace AillieoUtils.EasyAsync.Tests
         {
             string reason = "TestReject";
             int i = 0;
+            int j = 0;
             Promise promise = new Promise();
             promise.OnRejected(() => i++);
             Assert.AreEqual(i, 0);
             promise.Reject(reason);
             Assert.AreEqual(i, 1);
+            Assert.AreEqual(j, 0);
             Assert.AreEqual(promise.reason, reason);
             Assert.Catch(typeof(Exception), () => promise.Reject(reason));
+        }
+
+        [Test]
+        public static void TestThenNoValue()
+        {
+            Promise promise = new Promise();
         }
     }
 }
